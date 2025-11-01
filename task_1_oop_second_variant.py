23dc827b4109d0b4a0ddbdc03b30ddaeed7bd8eb
Pavell4599 <149090687+Pavell4599@users.noreply.github.com>
2025-11-01 13:10:36 +0200

    Финальное решение задачи 1 ООП

--- task_1_oop_second_variant.py
+++ task_1_oop_second_variant.py
@@ -1,27 +1,52 @@
-
-
 class Journal:
     
     
     def __init__(self, klass):
         self.klass = klass
-        self.stud_list = {}
+        self.class_journal = {}
+
     
-    
-    def add_student(self, stud_list, student):
-        self.student = student
-        self.stud_list[student] = {}
-        
-        
-    def add_subjects(self):
-        self.stud_list[]
+    def add_student(self, students):
+        for i in range(len(students)):
+            
+            self.class_journal[students[i]] = {}
         
     
+    def add_subjects(self, subject):
+        for student in self.class_journal:
+            self.class_journal[student][subject] = []
+
+
+    def rate_students(self, student, subject, mark):
+        self.class_journal[student][subject].append(mark)
     
-    def rate_student(self):
+
+    def print_statistics(self, student, subject):
+        print(self.class_journal[student][subject])
     
+
+    def print_middle(self, student, subject):
+        print(round(sum(self.class_journal[student][subject]) / len(self.class_journal[student][subject]), 2))
+
         
-    def print_statistics(self):
+    def test(self):
+        print(self.class_journal)
     
     
-    def print_middle(self):
+    
+clas1=Journal('10-I')
+clas1.add_student(['Пётр Васин', 'Вовочка', 'Антон'])
+clas1.add_subjects('Английский язык')
+clas1.add_subjects('Программирование')
+
+clas1.rate_students('Вовочка', 'Программирование', 4)
+clas1.rate_students('Вовочка', 'Программирование', 5)
+clas1.rate_students('Вовочка', 'Программирование', 3)
+clas1.rate_students('Вовочка', 'Программирование', 5)
+clas1.rate_students('Вовочка', 'Программирование', 5)
+clas1.rate_students('Вовочка', 'Программирование', 1)
+
+clas1.print_statistics('Вовочка', 'Программирование')
+clas1.print_middle('Вовочка', 'Программирование')
+
+
