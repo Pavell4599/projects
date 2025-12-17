f1c3e66acb104434ddc670d8e5471280467e9c05
Pavell4599 <149090687+Pavell4599@users.noreply.github.com>
2025-12-17 17:14:57 +0200

    задача 2

--- test_1.py
+++ test_1.py
@@ -1,34 +1,27 @@
-
-
-# Implementation of matplotlib function
 import matplotlib.pyplot as plt
-import matplotlib.tri as tri
-import numpy as np 
-     
-ang = 40
-rad = 10
-radm = 0.35
-radii = np.linspace(radm, 0.95, rad)
-     
-angles = np.linspace(0, 0.5 * np.pi, ang)
-angles = np.repeat(angles[..., np.newaxis], 
-                   rad, axis = 1)
-  
-angles[:, 1::2] += np.pi / ang
-     
-x = (radii * np.cos(angles)).flatten()
-y = (radii * np.sin(angles)).flatten()
-z = (np.sin(4 * radii) * np.cos(4 * angles)).flatten()
-     
-triang = tri.Triangulation(x, y)
-triang.set_mask(np.hypot(x[triang.triangles].mean(axis = 1),
-                         y[triang.triangles].mean(axis = 1))
-                < radm)
-     
-tpc = plt.tripcolor(triang, z, shading ="flat")
-plt.colorbar(tpc)
-plt.flag()
-plt.title("matplotlib.pyplot.flag() function Example ",
-          fontweight ="bold")
-  
-plt.savefig('flag.png')
+import numpy as np
+
+def circle_plotter(radius = 10):
+    
+    
+    x = np.arange(-2 * radius, 2 * radius, 0.1)
+    y = np.arange(-2 * radius, 2 * radius, 0.1)
+    
+    X, Y = np.meshgrid(x, y)
+   
+    fxy = X**2 + Y**2 #- radius**2
+   
+    plt.contour(X, Y, fxy, levels = [1, 2, 3, 10, 20], lw = 5)
+    
+    
+    plt.xlabel('Coord: x')
+    plt.ylabel('Coord: y')
+    plt.title('Circle')
+    plt.legend()
+    plt.axis('equal')
+
+    
+    plt.savefig('fig_4.png')
+    
+if __name__ == '__main__':
+    circle_plotter()
