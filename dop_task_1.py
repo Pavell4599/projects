cdd53783be8530ed8174f40dc4a2e16e783cb160
Pavell4599 <149090687+Pavell4599@users.noreply.github.com>
2026-01-24 15:03:35 +0200

    циклоида

--- dop_task_1.py
+++ dop_task_1.py
@@ -4,40 +4,64 @@ from matplotlib.animation import FuncAnimation
 
 
 
-def cicloida():
-    fig, ax = plt.subplots()
-    
-    
-    anim_object, = plt.plot([], [], '-', lw=1)
-    plt.axis('equal')
-    x, y = [], []
+fig, ax = plt.subplots()
 
-    t = np.linspace(0, 2*np.pi, 120)
-    R = 1
-    
-    ax.set_xlim(-2, 9) 
-    ax.set_ylim(-2, 9) 
-    
-    
-    def update(t, r = R):
-        xapp = r * t - r * np.sin(t)
-        yapp = r - r * np.cos(t)
-        x.append(xapp)
-        y.append(yapp)
-        
-        
-        
-        anim_object.set_data(x, y)
-    
-        return anim_object
-    
-    
-    ani = FuncAnimation(fig, 
-                        update, 
-                        frames=t, 
-                        interval=50) 
-    
-    ani.save('cicloida.gif', writer="pillow")
-    plt.close()
+cycloida, = plt.plot([], [], '-', lw=1)
+circle, = plt.plot([], [], '-', lw=1, color = 'green')
+rad, = plt.plot([], [], '-', lw=2, color = 'red')
+plt.axis('equal')
+x, y = [], []
 
-cicloida()
+
+frames_count = 200
+R = 1.5
+circle_count = 8
+
+frames = np.arange(frames_count)
+angle = np.linspace(0, circle_count * np.pi, frames_count)
+
+last_angle = circle_count * np.pi
+x_offset = np.linspace(0, (R * last_angle - R * np.sin(last_angle)), frames_count)
+
+ax.set_xlim(-10, 35) 
+ax.set_ylim(-3, 16) 
+
+
+def cycloida_fr(frame):
+    r = R
+    t = angle[frame]
+    xapp = r * t - r * np.sin(t)
+    yapp = r - r * np.cos(t)
+    x.append(xapp)
+    y.append(yapp)
+    
+    x_rad = [x_offset[frame], xapp]
+    y_rad = [r, yapp]
+    
+    rad.set_data(x_rad, y_rad)
+    cycloida.set_data(x, y)
+    return cycloida, rad
+
+
+def circle_fr(frame):
+    r = R
+    alpha = np.arange(-0.1, 2 * np.pi, 0.1)
+    x = r * np.cos(alpha) + x_offset[frame]
+    y = r * np.sin(alpha) + r
+
+    circle.set_data(x, y)
+    return circle
+    
+
+def update(t):
+    return cycloida_fr(t), circle_fr(t)
+
+
+ani = FuncAnimation(fig, 
+                    update, 
+                    frames=frames, 
+                    interval= 20) 
+
+ani.save('cycloida.gif', writer="pillow")
+plt.close()
+
--- test_2.py
+++ test_2.py
@@ -1,5 +1,44 @@
-a = [9, 99, 999, 9999, 999, 99, 9]
-for el in a:
-    el = el - 10
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-print(a)
+
+
+def cicloida():
+    fig, ax = plt.subplots()
+    
+    
+    anim_object, = plt.plot([], [], '-', lw=1)
+    plt.axis('equal')
+    x, y = [], []
+    
+    t = np.linspace(0, 8*np.pi, 200)
+    R = 0.3
+    
+    ax.set_xlim(-1, 9) 
+    ax.set_ylim(-1, 5) 
+    
+    
+    def update(t):
+        r = R
+        xapp = r * t - r * np.sin(t)
+        yapp = r - r * np.cos(t)
+        x.append(xapp)
+        y.append(yapp)
+        
+        
+        
+        anim_object.set_data(x, y)
+    
+        return anim_object
+    
+    
+    ani = FuncAnimation(fig, 
+                        update, 
+                        frames=t, 
+                        interval= 20) 
+    
+    ani.save('cycloida.gif', writer="pillow")
+    plt.close()
+
+cicloida()
